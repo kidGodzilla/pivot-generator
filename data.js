0bc8827b227a4bf08dd2ab54d13097e9052ced04
James Futhey <kidGodzilla@users.noreply.github.com>
2021-07-22 12:05:59 -0700

    first

---
+++ data.js
@@ -0,0 +1,305 @@
+var source_data = [
+    {
+        "region": "East",
+        "sales_rep": "Jones",
+        "item": "Pencil",
+        "item_category": "1",
+        "sales": 189.05
+    },
+    {
+        "region": "Central",
+        "sales_rep": "Kivell",
+        "item": "Binder",
+        "item_category": "2",
+        "sales": 999.5
+    },
+    {
+        "region": "Central",
+        "sales_rep": "Jardine",
+        "item": "Pencil",
+        "item_category": "3",
+        "sales": 179.64
+    },
+    {
+        "region": "Central",
+        "sales_rep": "Gill",
+        "item": "Pen",
+        "item_category": "3",
+        "sales": 539.73
+    },
+    {
+        "region": "West",
+        "sales_rep": "Sorvino",
+        "item": "Pencil",
+        "item_category": "2",
+        "sales": 167.44
+    },
+    {
+        "region": "East",
+        "sales_rep": "Jones",
+        "item": "Binder",
+        "item_category": "2",
+        "sales": 299.4
+    },
+    {
+        "region": "Central",
+        "sales_rep": "Andrews",
+        "item": "Pencil",
+        "item_category": "1",
+        "sales": 149.25
+    },
+    {
+        "region": "Central",
+        "sales_rep": "Jardine",
+        "item": "Pencil",
+        "item_category": "2",
+        "sales": 449.1
+    },
+    {
+        "region": "West",
+        "sales_rep": "Thompson",
+        "item": "Pencil",
+        "item_category": "0",
+        "sales": 63.68
+    },
+    {
+        "region": "East",
+        "sales_rep": "Jones",
+        "item": "Binder",
+        "item_category": "3",
+        "sales": 539.4
+    },
+    {
+        "region": "Central",
+        "sales_rep": "Morgan",
+        "item": "Pencil",
+        "item_category": "3",
+        "sales": 449.1
+    },
+    {
+        "region": "East",
+        "sales_rep": "Howard",
+        "item": "Binder",
+        "item_category": "3",
+        "sales": 57.71
+    },
+    {
+        "region": "East",
+        "sales_rep": "Parent",
+        "item": "Binder",
+        "item_category": "3",
+        "sales": 1619.19
+    },
+    {
+        "region": "East",
+        "sales_rep": "Jones",
+        "item": "Pencil",
+        "item_category": "0",
+        "sales": 174.65
+    },
+    {
+        "region": "Central",
+        "sales_rep": "Smith",
+        "item": "Desk",
+        "item_category": "3",
+        "sales": 250
+    },
+    {
+        "region": "East",
+        "sales_rep": "Jones",
+        "item": "Pen Set",
+        "item_category": "2",
+        "sales": 255.84
+    },
+    {
+        "region": "Central",
+        "sales_rep": "Morgan",
+        "item": "Binder",
+        "item_category": "3",
+        "sales": 251.72
+    },
+    {
+        "region": "East",
+        "sales_rep": "Jones",
+        "item": "Pen",
+        "item_category": "2",
+        "sales": 575.36
+    },
+    {
+        "region": "East",
+        "sales_rep": "Parent",
+        "item": "Pen",
+        "item_category": "1",
+        "sales": 299.85
+    },
+    {
+        "region": "Central",
+        "sales_rep": "Kivell",
+        "item": "Pen Set",
+        "item_category": "0",
+        "sales": 479.04
+    },
+    {
+        "region": "Central",
+        "sales_rep": "Smith",
+        "item": "Pencil",
+        "item_category": "0",
+        "sales": 86.43
+    },
+    {
+        "region": "East",
+        "sales_rep": "Parent",
+        "item": "Pen Set",
+        "item_category": "0",
+        "sales": 1183.26
+    },
+    {
+        "region": "Central",
+        "sales_rep": "Gill",
+        "item": "Binder",
+        "item_category": "1",
+        "sales": 413.54
+    },
+    {
+        "region": "Central",
+        "sales_rep": "Smith",
+        "item": "Binder",
+        "item_category": "1",
+        "sales": 1305
+    },
+    {
+        "region": "East",
+        "sales_rep": "Jones",
+        "item": "Binder",
+        "item_category": "1",
+        "sales": 19.96
+    },
+    {
+        "region": "West",
+        "sales_rep": "Sorvino",
+        "item": "Binder",
+        "item_category": "2",
+        "sales": 139.93
+    },
+    {
+        "region": "Central",
+        "sales_rep": "Jardine",
+        "item": "Pen Set",
+        "item_category": "3",
+        "sales": 249.5
+    },
+    {
+        "region": "Central",
+        "sales_rep": "Andrews",
+        "item": "Pencil",
+        "item_category": "0",
+        "sales": 131.34
+    },
+    {
+        "region": "East",
+        "sales_rep": "Howard",
+        "item": "Pen",
+        "item_category": "1",
+        "sales": 479.04
+    },
+    {
+        "region": "Central",
+        "sales_rep": "Gill",
+        "item": "Pencil",
+        "item_category": "2",
+        "sales": 68.37
+    },
+    {
+        "region": "Central",
+        "sales_rep": "Gill",
+        "item": "Binder",
+        "item_category": "3",
+        "sales": 719.2
+    },
+    {
+        "region": "Central",
+        "sales_rep": "Kivell",
+        "item": "Desk",
+        "item_category": "1",
+        "sales": 625
+    },
+    {
+        "region": "East",
+        "sales_rep": "Jones",
+        "item": "Pen Set",
+        "item_category": "3",
+        "sales": 309.38
+    },
+    {
+        "region": "Central",
+        "sales_rep": "Morgan",
+        "item": "Pen Set",
+        "item_category": "2",
+        "sales": 686.95
+    },
+    {
+        "region": "Central",
+        "sales_rep": "Kivell",
+        "item": "Pen Set",
+        "item_category": "2",
+        "sales": 1005.9
+    },
+    {
+        "region": "West",
+        "sales_rep": "Sorvino",
+        "item": "Desk",
+        "item_category": "3",
+        "sales": 825
+    },
+    {
+        "region": "Central",
+        "sales_rep": "Gill",
+        "item": "Pencil",
+        "item_category": "3",
+        "sales": 9.03
+    },
+    {
+        "region": "West",
+        "sales_rep": "Sorvino",
+        "item": "Pen",
+        "item_category": "0",
+        "sales": 151.24
+    },
+    {
+        "region": "West",
+        "sales_rep": "Thompson",
+        "item": "Binder",
+        "item_category": "2",
+        "sales": 1139.43
+    },
+    {
+        "region": "Central",
+        "sales_rep": "Andrews",
+        "item": "Pencil",
+        "item_category": "2",
+        "sales": 18.06
+    },
+    {
+        "region": "Central",
+        "sales_rep": "Jardine",
+        "item": "Binder",
+        "item_category": "1",
+        "sales": 54.89
+    },
+    {
+        "region": "Central",
+        "sales_rep": "Jardine",
+        "item": "Binder",
+        "item_category": "2",
+        "sales": 1879.06
+    },
+    {
+        "region": "Central",
+        "sales_rep": "Andrews",
+        "item": "Binder",
+        "item_category": "0",
+        "sales": 139.72
+    }
+]
+
+export default source_data;
